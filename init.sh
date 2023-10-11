6325fad73c24f6264b5c60edefa13764d641267a
agarkovpa <pavel.agarkov@vitech.team>
2023-10-11 22:48:10 +0600

    nginx client_max_body_size

--- init.sh
+++ init.sh
@@ -9,3 +9,15 @@ apt install docker.io
 apt install docker-compose
 apt install net-tools
 cd /var/www/ && git clone https://github.com/PavelAgarkov/pdf.git && cd pdf/ && git clone https://github.com/PavelAgarkov/pdf-frontend.git
+
+
+cd /home
+curl -OL https://golang.org/dl/go1.21.3.linux-amd64.tar.gz
+cd /usr/local/go
+
+nano ~/.profile
+#export PATH=$PATH:/usr/local/go/bin
+cd /var/www/pdf
+source ~/.profile
+
+go build && go mod vendor && go mod tidy
