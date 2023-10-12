dac433b58541e008f988853342c04ccb9b14157a
agarkovpa <pavel.agarkov@vitech.team>
2023-10-12 19:53:56 +0600

    prod.sh final

--- prod.sh
+++ prod.sh
@@ -1,68 +1,64 @@
 #!/bin/bash -e
 
-#anysync() {
-#    apt-get update
-#      apt install git
-#  cd ~/.ssh && ssh-keygen
-#  ssh-keygen -t rsa
-#   eval "$(ssh-agent -s)"
-#   ssh-add ~/.ssh/id_ed25519
-#   git remote set-url origin git@github.com:PavelAgarkov/pdf-frontend.git
-
-
-#  /etc/init.d/apache2 stop
-#  update-rc.d apache2 disable
-
-#  apt install docker.io
-#  apt install docker-compose
-#  apt install net-tools
-
-
-#  cd /home
-#  curl -OL https://golang.org/dl/go1.21.3.linux-amd64.tar.gz
-#  cd /usr/local/go
-
-#  nano ~/.profile
-  #export PATH=$PATH:/usr/local/go/bin
-#  cd /var/www/pdf
-#  source ~/.profile
-
-#  go build && go mod vendor && go mod tidy
-#}
+command() {
+    echo "Укажите команду при запуске: ./help.sh [command]" &&
+    echo "git_init - инициализирует гит и проект, выкачивает и устанавливает права на фронтенд" &&
+    echo "ssh_gen - геренерирует пару ssh и добавляет в агента"  &&
+    echo "git_update - обновляет изменения в мастер ветках бэка и фронта" &&
+    echo "build_project - выполняет сборку фронтенда и бэкенда" &&
+    echo "backend_build - сборка только бэкенда" &&
+    echo "frontend_build - сборка только фронтенда" &&
+    echo "go_install - выкачивает го 1.21.3 и распаковывает его с добавлением в пас" &&
+    echo "git_install - скачивает гит" &&
+    echo "apache2_stop - останавливает апач2 и выводит из автозагрузки, чтобы не подавлял nginx в контейнере" &&
+    echo "start_service - запускает сервис со всеми обновления и пересборками" &&
+    echo "stop_service - останавливает все контенеры" &&
+    echo "docker_install - устанавливает docker и docker-compose"
+}
 
 git_init() {
-        apt install git &&
-           cd ~/.ssh
-#           ssh-keygen -t rsa &&
-#            eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_ed25519
-#  cd /var/www/ && git clone git@github.com:PavelAgarkov/pdf.git && cd pdf/ && git clone git@github.com:PavelAgarkov/pdf-frontend.git
+  apt install git &&
+  cd /var/www/ &&
+  git clone git@github.com:PavelAgarkov/pdf.git &&
+  cd pdf/ &&
+  git clone git@github.com:PavelAgarkov/pdf-frontend.git &&
+  chmod 666 /var/www/pdf/pdf-frontend
+}
 
-#chmod -r 666 /var/www/pdf/pdf-frontend
+ssh_gen() {
+  cd ~/.ssh &&
+  ssh-keygen -t rsa &&
+   eval "$(ssh-agent -s)"
+#   ssh-add ~/.ssh/id_ed25519
 }
 
 git_update() {
   cd /var/www/pdf &&
   git pull &&
+  echo "backend pull complete\n" &&
   cd /var/www/pdf/pdf-frontend/ &&
-  git pull
+  git pull &&
+  echo "frontend pull complete\n"
 }
 
 build_project() {
-  backend_build && frontend_build
+  frontend_build && backend_build
 }
 
 backend_build() {
   cd /var/www/pdf &&
        go build &&
        go mod vendor &&
-       go mod tidy
+       go mod tidy &&
+       echo "backend build complete\n"
 }
 
 frontend_build() {
   docker-compose -f /var/www/pdf/docker-compose-prode.yaml start node &&
    docker exec node-local npm install &&
   docker exec node-local npm run build &&
-  docker-compose -f /var/www/pdf/docker-compose-prode.yaml stop node
+  docker-compose -f /var/www/pdf/docker-compose-prode.yaml stop node &&
+  echo "frontend build complete\n"
 }
 
 go_install() {
@@ -83,19 +79,24 @@ git_install() {
 
 apache2_stop() {
     /etc/init.d/apache2 stop &&
-    update-rc.d apache2 disable
+    update-rc.d apache2 disable &&
+    echo "apache2 stopped complete\n"
 }
 
 start_service() {
- stop_service &&
+  apache2_stop &&
+  git_update &&
+  stop_service &&
   docker-compose -f /var/www/pdf/docker-compose-prode.yaml build &&
-  frontend_build && backend_build &&
+  build_project &&
   docker-compose -f /var/www/pdf/docker-compose-prode.yaml up -d &&
+  echo "service start on port 80\n" &&
   docker-compose -f /var/www/pdf/docker-compose-prode.yaml stop node
 }
 
 stop_service() {
-  docker-compose -f /var/www/pdf/docker-compose-prode.yaml stop
+  docker-compose -f /var/www/pdf/docker-compose-prode.yaml stop &&
+  echo "service stopped complete\n"
 }
 
 docker_install() {
